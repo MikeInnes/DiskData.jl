a431345682184c5dfc50863b4b09758d2f2bc7e7
Mike Innes <mike.j.innes@gmail.com>
2015-12-23 00:36:36 +0000

    chunk size as type parameter

--- src/vectors/chunked.jl
+++ src/vectors/chunked.jl
@@ -1,21 +1,26 @@
-immutable ChunkedVector{T,A<:AVector} <: AVector{T}
-  size::Int
+immutable ChunkedVector{T,A<:AVector,N} <: AVector{T}
   data::Vector{A}
 end
 
-typealias ChunkedVectorT{T,A} Type{ChunkedVector{T,A}}
+typealias ChunkedVectorT{T,A,N} Type{ChunkedVector{T,A,N}}
 
-call{T,A}(::ChunkedVectorT{T,A}, size = 128, data = A[A()]) =
-  ChunkedVector{T,A}(size, data)
+call{T,A,N}(::ChunkedVectorT{T,A,N}, data = A[A()]) =
+  ChunkedVector{T,A,N}(data)
 
-call{T}(::ChunkedVectorT{T}, a...) = ChunkedVector{T,Vector{T}}(a...)
+call{T,A}(::ChunkedVectorT{T,A}, data = A[A()]) =
+  ChunkedVector{T,A,128}(data)
+
+call{T}(::ChunkedVectorT{T}, a...) =
+  ChunkedVector{T,Vector{T}}(a...)
+
+chunksize{T,A,N}(::ChunkedVector{T,A,N}) = N
 
 function makeroom!(xs::ChunkedVector)
   push!(xs.data, similar(xs.data[end], 0))
 end
 
 function Base.push!(xs::ChunkedVector, y)
-  length(xs.data[end]) == xs.size && makeroom!(xs)
+  length(xs.data[end]) == chunksize(xs) && makeroom!(xs)
   push!(xs.data[end], y)
   return xs
 end
@@ -29,7 +34,7 @@ function ChunkedVector(xs, a...)
 end
 
 function index(xs::ChunkedVector, i)
-  j, i′ = divrem(i-1, xs.size)
+  j, i′ = divrem(i-1, chunksize(xs))
   j + 1, i′ + 1
 end
 
@@ -43,6 +48,6 @@ function Base.setindex!(xs::ChunkedVector, v, i::Integer)
   xs.data[j][i′] = v
 end
 
-Base.length(xs::ChunkedVector) = xs.size*(length(xs.data)-1)+length(xs.data[end])
+Base.length(xs::ChunkedVector) = chunksize(xs)*(length(xs.data)-1)+length(xs.data[end])
 
 Base.size(xs::ChunkedVector) = (length(xs),)

4c091b0e11540a16bc3f171583097791f74932a9
Mike Innes <mike.j.innes@gmail.com>
2015-12-24 15:05:55 +0000

    nested iterators

--- src/iter/iter.jl
+++ src/iter/iter.jl
@@ -19,3 +19,29 @@ macro iter(ex, it)
     end
   end |> esc
 end
+
+immutable NestedIter{I}
+  it::I
+end
+
+@inline function Base.start(i::NestedIter)
+  state = start(i.it)
+  @assert !done(i.it, state)
+  sub, state = next(i.it, state)
+  state, sub, start(sub)
+end
+
+@inline function Base.done(i::NestedIter, s)
+  state, sub, substate = s
+  done(sub, substate) && done(i.it, state)
+end
+
+@inline function Base.next(i::NestedIter, s)
+  state, sub, substate = s
+  if done(sub, substate)
+    sub, state = next(i.it, state)
+    substate = start(sub)
+  end
+  x, substate = next(sub, substate)
+  x, (state, sub, substate)
+end
